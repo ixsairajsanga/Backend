5f523b3fa5b6fbccf7c12e306641b48a3b6d87e0
sairajsanga <sairajsanga459@gmail.com>
2026-03-16 11:26:32 +0530

    springDay 5

---
+++ 12-03-26/spring_day5/src/main/java/com/example/spring_day5/Entity/Student.java
@@ -0,0 +1,48 @@
+package com.example.spring_day5.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column
+    private Integer age;
+
+    public Student(Long id,String Name,Integer age){
+        this.id=id;
+        this.name=Name;
+        this.age=age;
+    }
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
